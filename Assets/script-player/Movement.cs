a02d132a6edda65a325cdccb2141107e3188e99f
Larisa-Condrache <larisa.condrache04@e-uvt.ro>
2026-06-22 02:37:34 +0300

    weapons and attacks done

--- Assets/script-player/Movement.cs
+++ Assets/script-player/Movement.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -17,29 +18,47 @@ public class PlayerMovement : MonoBehaviour
 
     public bool isDead = false;
 
+    public float knifeAttackDuration = 0.45f;
+    public float pistolAttackDuration = 0.35f;
+    public float rifleAttackDuration = 0.4f;
+
     private Rigidbody2D rb;
     private float moveInput;
     private bool isGrounded;
     private float currentSpeed;
+    private bool isAttacking = false;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         currentSpeed = runSpeed;
+
+        UpdateWeaponAnimator();
     }
 
     void Update()
     {
+        UpdateWeaponAnimator();
+
         if (isDead)
         {
             moveInput = 0f;
-
-            animator.SetBool("isWalking", false);
-            animator.SetBool("isRunning", false);
-            animator.SetBool("isJumping", false);
+            ResetMovementAnimations();
             animator.SetBool("isDead", true);
+            return;
+        }
 
+        if (Input.GetKeyDown(KeyCode.J) &&
+            WeaponManager.Instance.currentWeapon != 0 &&
+            !isAttacking)
+        {
+            StartCoroutine(Attack());
+        }
+
+        if (isAttacking)
+        {
+            moveInput = 0f;
             return;
         }
 
@@ -52,13 +71,9 @@ public class PlayerMovement : MonoBehaviour
         bool isRunning = isMoving && !Input.GetKey(KeyCode.LeftControl);
 
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-        {
             transform.localScale = new Vector3(1, 1, 1);
-        }
         else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
-        {
             transform.localScale = new Vector3(-1, 1, 1);
-        }
 
         animator.SetBool("isWalking", isWalking);
         animator.SetBool("isRunning", isRunning);
@@ -72,9 +87,7 @@ public class PlayerMovement : MonoBehaviour
         animator.SetBool("isJumping", !isGrounded);
 
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-        {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
-        }
 
         currentSpeed = isWalking ? stealthSpeed : runSpeed;
     }
@@ -87,19 +100,66 @@ public class PlayerMovement : MonoBehaviour
             return;
         }
 
-        float targetSpeed = moveInput * currentSpeed;
+        if (isAttacking)
+        {
+            rb.linearVelocity = new Vector2(0f, rb.linearVelocity.y);
+            return;
+        }
 
+        float targetSpeed = moveInput * currentSpeed;
         rb.linearVelocity = new Vector2(targetSpeed, rb.linearVelocity.y);
 
         if (rb.linearVelocity.y < 0)
         {
             rb.linearVelocity += Vector2.up * Physics2D.gravity.y *
-                               (fallMultiplier - 1) * Time.fixedDeltaTime;
+                                 (fallMultiplier - 1) * Time.fixedDeltaTime;
         }
         else if (rb.linearVelocity.y > 0 && !Input.GetKey(KeyCode.Space))
         {
             rb.linearVelocity += Vector2.up * Physics2D.gravity.y *
-                               (lowJumpMultiplier - 1) * Time.fixedDeltaTime;
+                                 (lowJumpMultiplier - 1) * Time.fixedDeltaTime;
         }
+    }
+
+    void UpdateWeaponAnimator()
+    {
+        if (WeaponManager.Instance == null)
+            return;
+
+        animator.SetInteger("weaponType", WeaponManager.Instance.currentWeapon);
+    }
+
+    IEnumerator Attack()
+    {
+        isAttacking = true;
+        moveInput = 0f;
+
+        ResetMovementAnimations();
+
+        int weapon = WeaponManager.Instance.currentWeapon;
+
+        animator.SetInteger("weaponType", weapon);
+        animator.SetBool("isAttacking", true);
+
+        float duration = 0.4f;
+
+        if (weapon == 1)
+            duration = knifeAttackDuration;
+        else if (weapon == 2)
+            duration = pistolAttackDuration;
+        else if (weapon == 3)
+            duration = rifleAttackDuration;
+
+        yield return new WaitForSeconds(duration);
+
+        animator.SetBool("isAttacking", false);
+        isAttacking = false;
+    }
+
+    void ResetMovementAnimations()
+    {
+        animator.SetBool("isWalking", false);
+        animator.SetBool("isRunning", false);
+        animator.SetBool("isJumping", false);
     }
 }
--- Assets/script-player/WeaponManager.cs
+++ Assets/script-player/WeaponManager.cs
@@ -7,29 +7,40 @@ public class WeaponManager : MonoBehaviour
     public Button pistolButton;
     public Button rifleButton;
 
-    private int currentWeapon = 3;
+    public int currentWeapon = 0;
+    // 0 = none
+    // 1 = knife
+    // 2 = pistol
+    // 3 = rifle
+
+    public static WeaponManager Instance;
+
+    void Awake()
+    {
+        Instance = this;
+    }
 
     void Start()
     {
-        SelectWeapon(3);
+        SelectWeapon(0);
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Alpha1))
-            SelectWeapon(0);
-
-        if (Input.GetKeyDown(KeyCode.Alpha2))
             SelectWeapon(1);
 
-        if (Input.GetKeyDown(KeyCode.Alpha3))
+        if (Input.GetKeyDown(KeyCode.Alpha2))
             SelectWeapon(2);
 
-        if (Input.GetKeyDown(KeyCode.Alpha4))
+        if (Input.GetKeyDown(KeyCode.Alpha3))
             SelectWeapon(3);
+
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+            SelectWeapon(0);
     }
 
-    void SelectWeapon(int index)
+    public void SelectWeapon(int index)
     {
         currentWeapon = index;
 
@@ -39,13 +50,33 @@ public class WeaponManager : MonoBehaviour
 
         Color selected = new Color(0.6f, 0.6f, 0.6f);
 
-        if (index == 0)
+        if (index == 1)
             knifeButton.image.color = selected;
 
-        if (index == 1)
+        if (index == 2)
             pistolButton.image.color = selected;
 
-        if (index == 2)
+        if (index == 3)
             rifleButton.image.color = selected;
+    }
+
+    public void SelectKnife()
+    {
+        SelectWeapon(1);
+    }
+
+    public void SelectPistol()
+    {
+        SelectWeapon(2);
+    }
+
+    public void SelectRifle()
+    {
+        SelectWeapon(3);
+    }
+
+    public void SelectNoWeapon()
+    {
+        SelectWeapon(0);
     }
 }
