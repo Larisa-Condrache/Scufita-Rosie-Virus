d639f72a8fae9aea7c72ad08eba9bcdf396dbb85
Larisa-Condrache <larisa.condrache04@e-uvt.ro>
2026-06-21 17:54:18 +0300

    implemented fully the main menu

--- Assets/scipt-mainmenu/OptionsMenuUI.cs
+++ Assets/scipt-mainmenu/OptionsMenuUI.cs
@@ -1,18 +1,57 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OptionsMenuUI : MonoBehaviour
 {
+    [Header("Panels")]
     public GameObject mainMenuPanel;
     public GameObject optionsPanel;
+
+    [Header("Volume")]
+    public Slider volumeSlider;
+
+    private float lastVolume = 1f;
+
+    void Start()
+    {
+        volumeSlider.value = AudioListener.volume;
+    }
 
     public void SetVolume(float volume)
     {
         AudioListener.volume = volume;
+
+        if (volume > 0f)
+            lastVolume = volume;
+    }
+
+    public void ToggleMute()
+    {
+        if (AudioListener.volume > 0f)
+        {
+            lastVolume = AudioListener.volume;
+            AudioListener.volume = 0f;
+            volumeSlider.value = 0f;
+        }
+        else
+        {
+            AudioListener.volume = lastVolume;
+            volumeSlider.value = lastVolume;
+        }
     }
 
     public void SetFullscreen(bool isFullscreen)
     {
-        Screen.fullScreen = isFullscreen;
+        if (isFullscreen)
+        {
+            Screen.fullScreenMode = FullScreenMode.FullScreenWindow;
+            Screen.fullScreen = true;
+        }
+        else
+        {
+            Screen.fullScreenMode = FullScreenMode.Windowed;
+            Screen.fullScreen = false;
+        }
     }
 
     public void ReturnToMainMenu()
