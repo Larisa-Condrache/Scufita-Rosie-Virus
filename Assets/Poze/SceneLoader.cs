928e1dea1273a582f9aa61e30d4869e86951f13c
CalotaAlexandra <alexitu.calota04@gmail.com>
2026-06-22 04:58:28 +0300

    nou4

---
+++ Assets/Poze/SceneLoader.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void NextScene()
+    {
+        SceneManager.LoadScene(
+            SceneManager.GetActiveScene().buildIndex + 1
+        );
+    }
+}
